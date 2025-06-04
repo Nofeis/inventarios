120de30b514c0abf903df8e2ea8c3ecfb92f046f
unknown <ismejia@uts.edu.co>
2025-06-04 11:06:39 -0500

    Eliminacion del .env del repo y agregado al gitignore

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 // Importando el framework express
 import express from "express";
 import categoriasRoutes from "./routes/categorias.routes.js";
+import cors from "cors";
 
 // Asignamos a app toda funcionalidad para mi server web
 const app = express();
@@ -10,6 +11,7 @@ app.set("port", 5000);
 
 // Routes
 app.use("/api/categorias", categoriasRoutes);
+app.use(cors());
 
 // Hacemos disponible a mi server app para toda la aplicacion
 export default app;
